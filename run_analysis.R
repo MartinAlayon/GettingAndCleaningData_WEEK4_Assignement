6446be4bf2d57cecc4470a1ca962880874c8e035
MartinAlayon <martin_alayon@yahoo.com>
2021-05-13 09:27:47 -0300

    Read data

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,16 @@
 
-##
-setwd("~/R/Scripts/GettingAndCleaningData/PeerA/UCI HAR Dataset")
+##Read files
 
+##READ ME HERE:Set your working directory as "whereveryourdatais/UCI HAR Dataset"
+#setwd("~/R/Scripts/GettingAndCleaningData/PeerA/UCI HAR Dataset")
+
+x_train<-read.csv("./train/x_train.txt")
+y_train<-read.csv("./train/y_train.txt")
+subject_train<-read.csv("./train/subject_train.txt")
+
+x_test<-read.csv("./test/x_train.txt")
+y_test<-read.csv("./test/y_train.txt")
+subject_test<-read.csv("./test/subject_train.txt")
 
 
 
