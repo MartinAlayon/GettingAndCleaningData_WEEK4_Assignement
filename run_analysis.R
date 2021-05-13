1f9bf2d313f1487a2623b4c8779583995c4be559
MartinAlayon <martin_alayon@yahoo.com>
2021-05-13 09:40:27 -0300

    Fixed reading files

--- run_analysis.R
+++ run_analysis.R
@@ -4,14 +4,20 @@
 ##READ ME HERE:Set your working directory as "whereveryourdatais/UCI HAR Dataset"
 #setwd("~/R/Scripts/GettingAndCleaningData/PeerA/UCI HAR Dataset")
 
-x_train<-read.csv("./train/x_train.txt")
-y_train<-read.csv("./train/y_train.txt")
-subject_train<-read.csv("./train/subject_train.txt")
+#Read training data
+x_train<-read.table("./train/x_train.txt",header = FALSE)
+y_train<-read.table("./train/y_train.txt",header = FALSE)
+subject_train<-read.table("./train/subject_train.txt",header = FALSE)
 
-x_test<-read.csv("./test/x_train.txt")
-y_test<-read.csv("./test/y_train.txt")
-subject_test<-read.csv("./test/subject_train.txt")
+#Read test data
+x_test<-read.table("./test/x_test.txt",header = FALSE)
+y_test<-read.table("./test/y_test.txt",header = FALSE)
+subject_test<-read.table("./test/subject_test.txt",header = FALSE)
 
+#Read features names
+features_names<-read.table("features.txt",header = FALSE)
 
+#Read activity labels
+activity_labels<-read.table("activity_labels.txt",header = FALSE)
 
 ##Merges the training and the test sets to create one data set
